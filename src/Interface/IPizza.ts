242e8fb4f5e42d9bf04ff02ab97be2cdccdc8885
bachtuyetx7chulun <contact.truongtrunghieu@gmail.com>
2021-02-21 22:20:52 +0700

    update

---
+++ src/Interface/IPizza.ts
@@ -0,0 +1,16 @@
+// interface IPizza {
+//   name: String;
+//   dougt: String;
+//   sauce: String;
+//   toppings: Array<String>;
+// }
+
+// const pizza: IPizza = {
+//   name: "123",
+//   dougt: "123",
+//   sauce: "123",
+//   toppings: ["123", "123"],
+// };
+
+
+// console.log(pizza);
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+interface IPizza {
+    name: String;
+    dougt: String;
+    sauce: String;
+    toppings: Array<String>;
+
+    Preparing: void;
+    Bake: void;
+    Cut: void;
+    Box: void;
+  }
+ 
+class Pizza implements IPizza {
+    Preparing: void 
+
+}  
+  
+ 
+  
